ed672b1d17d2316cd8773525b28f7ea8c706b230
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-01-24 00:40:20 +0100

    Add ground moving

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,52 @@
-import { useCallback, useState } from 'react';
-import { Stage, Graphics as GraphicsComp, useTick, Container } from '@pixi/react';
+import { useState } from 'react';
+import { Stage, useTick, Container, Sprite } from '@pixi/react';
 import './App.css';
-import { Graphics } from 'pixi.js';
+import groundTile from "./assets/tile_ground.png";
 
-function Ground({ appWidth, appHeight }) {
-  const [x1, setX1] = useState(appWidth);
-  const [y1, setY1] = useState(appHeight - 15);
-  const [x2, setX2] = useState(appWidth * 2);
-  const [y2, setY2] = useState(appHeight - 15);
+function Ground({ appWidth, appHeight, startX }: { appWidth: number, appHeight: number, startX: number }) {
+  const groundHeight = 36;
+  const groundWidth = 80;
 
-  const draw1 = useCallback(
-    (g: Graphics) => {
-      let tempX = x1;
-      let tempY = y1;
+  const [xPos, setXPos] = useState(startX);
 
-      g.clear()
-      .lineStyle(2, 0x000000, 1)
-      .moveTo(tempX, tempY);
-      tempY -= 20;
-      g.lineTo(tempX, tempY);
-      tempY += 20;
-      tempX += 150;
-      g.lineTo(tempX, tempY);
-      // tempX += 20;
-      // tempY -= 20;
-      // g.lineTo(tempX, tempY);
-      // tempX += 20;
-      // tempY += 20;
-      // g.lineTo(tempX, tempY);
-      tempX += appWidth - tempX;
-      g.lineTo(tempX, tempY);
-    },
-    [x1, y1],
-  );
-
-  const draw2 = useCallback(
-    (g: Graphics) => {
-      let tempX = x2;
-      let tempY = y2;
-
-      g.clear()
-      .lineStyle(2, 0x000000, 1)
-      .moveTo(tempX, tempY);
-      tempY -= 20;
-      g.lineTo(tempX, tempY);
-      tempY += 20;
-      tempX += 150;
-      g.lineTo(tempX, tempY);
-      // tempX += 20;
-      // tempY -= 20;
-      // g.lineTo(tempX, tempY);
-      // tempX += 20;
-      // tempY += 20;
-      // g.lineTo(tempX, tempY);
-      tempX += appWidth - tempX;
-      g.lineTo(tempX, tempY);
-    },
-    [x2, y2],
-  );
-
-
-  useTick(deltaTime => {
-    if (x1 <= -appWidth) {
-      setX1(appWidth);
-      return;
-    }
-
-    setX1(x1 - (deltaTime * 5));
-    setX2(x2 - (deltaTime * 5));
+  const groundTilesPartOne = Array.from({ length: 10 }, (_, i) => {
+    return {
+      i: i,
+      image: groundTile,
+      x: xPos + i * groundWidth,
+      y: appHeight - groundHeight + 4,
+      height: groundHeight,
+      width: groundWidth
+    };
   });
 
-  return <Container>
-    <GraphicsComp draw={draw1} />
-    <GraphicsComp draw={draw2} />
-  </Container>
+  useTick(d => {
+    let tempXPos = xPos - 3;
+
+    if (tempXPos <= -appWidth) {
+      tempXPos = appWidth;
+      setXPos(tempXPos);
+    }
+    else {
+      setXPos(tempXPos);
+    }
+  });
+
+  return (
+    <Container>
+      {groundTilesPartOne.map(gt => (
+        <Sprite
+          key={gt.i}
+          image={gt.image}
+          height={gt.height}
+          width={gt.width}
+          x={gt.x}
+          y={gt.y}
+        />
+      ))
+      }
+    </Container>
+  );
 }
 
 function App() {
@@ -81,8 +54,8 @@ function App() {
   const appHeight = 600;
   return (
     <Stage width={appWidth} height={appHeight} options={{ background: 0x1099bb }}>
-      <Ground appWidth={appWidth} appHeight={appHeight}/>
-      {/* <Ground2 appWidth={appWidth} appHeight={appHeight}/> */}
+      <Ground appHeight={appHeight} appWidth={appWidth} startX={0} />
+      <Ground appHeight={appHeight} appWidth={appWidth} startX={appWidth} />
     </Stage>
   );
 }
