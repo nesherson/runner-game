8c9e9fb71697ed9043577049a6f2e90140a0067d
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-01-27 12:42:27 +0100

    Add score component

---
+++ src/features/game/Score.tsx
@@ -0,0 +1,24 @@
+import { Text } from "@pixi/react";
+import { TextStyle } from "pixi.js";
+
+import { APP_WIDTH, APP_HEIGHT } from "./constants";
+
+interface Props {
+    score: number
+}
+
+function Score({ score }: Props) {
+    return (
+        <Text
+            text={Math.floor(score).toString()}
+            anchor={0.5}
+            x={APP_WIDTH * 0.85}
+            y={APP_HEIGHT * 0.1}
+            style={new TextStyle({
+                fontSize: 25
+            })}
+        />
+    );
+}
+
+export default Score;
