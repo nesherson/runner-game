ebf0ff85cd3d732c627d8dce657e407226771c90
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-04-29 15:10:31 +0200

    Fix app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { Stage, withPixiApp } from '@pixi/react';
+import { Application } from '@pixi/react';
 
 import './App.css';
+
 import Game from './features/game/components/Game';
 import { useGameSettingsStore } from './features/game/stores';
 
 function App() {
-  const Comp = withPixiApp(Game);
   const appWidth = useGameSettingsStore(state => state.appWidth);
   const appHeight = useGameSettingsStore(state => state.appHeight);
 
   return (
-    <Stage width={appWidth} height={appHeight} options={{ background: 0x1099bb }}>
-      <Comp />
-    </Stage>
+    <Application width={appWidth} height={appHeight} background={0x1099bb}>
+      <Game />
+    </Application>
   );
 }
 
