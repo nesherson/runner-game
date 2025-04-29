44854de4db17017e1437ac8e4e50a46c777b7e18
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-04-29 20:25:37 +0200

    Fix player

--- src/features/game/components/Player.tsx
+++ src/features/game/components/Player.tsx
@@ -1,24 +1,64 @@
-import { AnimatedSprite } from "@pixi/react";
+import {
+	AnimatedSprite,
+	Assets,
+	type Texture,
+	Text,
+	type TextureSource,
+} from "pixi.js";
 
 import type { Player as PlayerType } from "../types/types";
+import { extend } from "@pixi/react";
+import { useState, useEffect, useRef } from "react";
+
+extend({
+	AnimatedSprite,
+	Text,
+});
 
 interface Props {
-  player: PlayerType,
-  isGamePlaying: boolean
+	player: PlayerType;
+	isGamePlaying: boolean;
 }
 
 function Player({ player, isGamePlaying }: Props) {
-  return (
-    <AnimatedSprite
-      textures={player.textures}
-      height={player.height}
-      width={player.width}
-      x={player.x}
-      y={player.y}
-      animationSpeed={player.animationSpeed}
-      isPlaying={isGamePlaying}
-    />
-  );
+	const playerRef = useRef<AnimatedSprite>(null);
+	const [textures, setTextures] = useState<Texture<TextureSource<any>>[]>([]);
+
+	useEffect(() => {
+		if (textures.length === 0) {
+			Assets.load(player.textures).then((result) => {
+				const tempTextures = [];
+
+				for (const [_, value] of Object.entries(result)) {
+					tempTextures.push(value);
+				}
+
+				setTextures(tempTextures);
+			});
+		}
+	}, [player.textures, textures]);
+
+	useEffect(() => {
+		if (isGamePlaying && playerRef.current) {
+			playerRef.current.play();
+		}
+	}, [isGamePlaying]);
+
+	return (
+		<>
+			{textures.length > 0 && (
+				<pixiAnimatedSprite
+					ref={playerRef}
+					textures={textures}
+					height={player.height}
+					width={player.width}
+					x={player.x}
+					y={player.y}
+          animationSpeed={player.animationSpeed}
+				/>
+			)}
+		</>
+	);
 }
 
 export default Player;
