1be9cb4aa64928bff322a8566561a682279ab46e
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-04-29 20:45:05 +0200

    Fix score

--- src/features/game/components/Score.tsx
+++ src/features/game/components/Score.tsx
@@ -1,25 +1,29 @@
-import { Text } from "@pixi/react";
-import { TextStyle } from "pixi.js";
+import { useExtend } from "@pixi/react";
+import { Text, TextStyle } from "pixi.js";
 
 interface Props {
-    appWidth: number,
-    appHeight: number,
-    sizeScale: number,
-    score: number
+	appWidth: number;
+	appHeight: number;
+	sizeScale: number;
+	score: number;
 }
 
 function Score({ appWidth, appHeight, sizeScale, score }: Props) {
-    return (
-        <Text
-            text={Math.floor(score).toString()}
-            anchor={0.5}
-            x={appWidth * 0.9}
-            y={appHeight * 0.05}
-            style={new TextStyle({
-                fontSize: 10 * sizeScale
-            })}
-        />
-    );
+	useExtend({ Text });
+
+	return (
+		<pixiText
+			text={Math.floor(score).toString()}
+			anchor={0.5}
+			x={appWidth * 0.9}
+			y={appHeight * 0.05}
+			style={
+				new TextStyle({
+					fontSize: 10 * sizeScale,
+				})
+			}
+		/>
+	);
 }
 
 export default Score;
